63b82a16634bde6105998b017f646ebfe143a004
Ahmed Adel <62815241+ahmedadel2020-code@users.noreply.github.com>
2023-02-04 14:32:43 +0200

    style pin with masonry grid

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -7,16 +7,14 @@ import { Sidebar, UserProfile } from "../components";
 import { Link, Route, Routes } from "react-router-dom";
 import { userQuery } from "../utils/data";
 import Pins from "./Pins";
+import { fetchUser } from "../utils/fetchUser";
 
 function Home() {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
   const scrollRef = useRef(null);
 
-  const userInfo =
-    localStorage.getItem("user") !== "undefined"
-      ? JSON.parse(localStorage.getItem("user"))
-      : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.userId);
